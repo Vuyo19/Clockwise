8896f4e0b6c94f78e3ca2e3afe1fe402594d9b1f
jayymmanuel <emmanuelkianda@gmail.com>
2023-06-06 20:25:51 +0200

    added ui design to add_photo.xml

--- app/src/main/java/com/example/clockwise/AddPhoto.kt
+++ app/src/main/java/com/example/clockwise/AddPhoto.kt
@@ -1,4 +1,4 @@
-import android.content.Intent
+ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
--- app/src/main/java/com/example/clockwise/DisplayTask.kt
+++ app/src/main/java/com/example/clockwise/DisplayTask.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CurrentDay.newInstance] factory method to
+ * Use the [DisplayTask.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CurrentDay : Fragment() {
+class DisplayTask : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class CurrentDay : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current_day, container, false)
+        return inflater.inflate(R.layout.fragment_user_tasks, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class CurrentDay : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CurrentDay().apply {
+            DisplayTask().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
